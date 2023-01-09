0f0b2174d74323b059dda0977072fef7d8055104
Kento Nishi <kento24gs@outlook.com>
2023-01-08 17:47:58 -0800

    ui settings

--- src/components/settings/YtcFilterGeneral.svelte
+++ src/components/settings/YtcFilterGeneral.svelte
@@ -1,16 +1,46 @@
 <script lang="ts">
   import { Theme } from '../../ts/chat-constants';
-  import { theme } from '../../ts/storage';
-  import { exioDropdown } from 'exio/svelte';
+  import { theme, showProfileIcons, showUsernames, showTimestamps, showUserBadges } from '../../ts/storage';
+  import { exioCheckbox, exioDropdown } from 'exio/svelte';
   import '../../stylesheets/ui.css';
 </script> 
 
 <div class="settings-title big-text">Appearance</div>
 <div class="settings-content">
-  <span>Theme: </span>
-  <select use:exioDropdown bind:value={$theme}>
-    <option value={Theme.YOUTUBE}>Auto</option>
-    <option value={Theme.LIGHT}>Light</option>
-    <option value={Theme.DARK}>Dark</option>
-  </select>
+  <div class="setting-item" style="margin-top: 0px;">
+    <span>Theme: </span>
+    <select use:exioDropdown bind:value={$theme}>
+      <option value={Theme.YOUTUBE}>Auto</option>
+      <option value={Theme.LIGHT}>Light</option>
+      <option value={Theme.DARK}>Dark</option>
+    </select>
+  </div>
+  <div class="setting-item">
+    <input class="check" type="checkbox" use:exioCheckbox bind:checked={$showProfileIcons} id="show-profile-icons" />
+    <label for="show-profile-icons">Show Profile Icons</label>
+  </div>
+  <div class="setting-item">
+    <input class="check" type="checkbox" use:exioCheckbox bind:checked={$showTimestamps} id="show-timestamps" />
+    <label for="show-timestamps">Show Timestamps</label>
+  </div>
+  <div class="setting-item">
+    <input class="check" type="checkbox" use:exioCheckbox bind:checked={$showUsernames} id="show-usernames" />
+    <label for="show-usernames">Show Usernames</label>
+  </div>
+  <div class="setting-item">
+    <input class="check" type="checkbox" use:exioCheckbox bind:checked={$showUserBadges} id="show-user-badges" />
+    <label for="show-user-badges">Show User Badges</label>
+  </div>
 </div>
+
+<style>
+  .setting-item {
+    display: block;
+    margin-top: 10px;
+  }
+  .check {
+    display: inline-block;
+    vertical-align: bottom;
+    margin: 0px 5px;
+  }
+</style>
--- src/ts/storage.ts
+++ src/ts/storage.ts
@@ -85,7 +85,7 @@ export const isDark = derived(theme, ($theme) => {
 });
 export const currentProgress = writable(null as null | number);
 export const enableStickySuperchatBar = stores.addSyncStore('ytcf.enableStickySuperchatBar', false);
-export const enableHighlightedMentions = stores.addSyncStore('ytcf.enableHighlightedMentions', true);
+export const enableHighlightedMentions = stores.addSyncStore('ytcf.enableHighlightedMentions', false);
 export const lastOpenedVersion = stores.addSyncStore('ytcf.lastOpenedVersion', '');
 export const chatFilterPresets = stores.addSyncStore('ytcf.chatFilterPresets', [{
   filters: [],
