32f68fbcbb5c1491513abc4cea5861e81e7a9261
Kento Nishi <kento24gs@outlook.com>
2026-04-16 14:50:38 -0400

    changelog

--- src/components/changelog/YtcFilterChangelog.svelte
+++ src/components/changelog/YtcFilterChangelog.svelte
@@ -1,12 +1,1 @@
-<strong>Here's what's new in v{__VERSION__}:</strong>
-<ul class="list-disc list-inside">
-  <li class="ml-3.5">
-    Sync HyperChat backbone
-  </li>
-  <li class="ml-3.5">
-    Fix embed visuals
-  </li>
-  <li class="ml-3.5">
-    Hide leading @ in names
-  </li>
-</ul>
+Fix embed visuals / Hide leading @ in names
